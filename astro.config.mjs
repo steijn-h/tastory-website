3d3974c9451297fb697eb9c0045d3fbdb876deca
Steijn Holtkamp <steijn@mac.lan>
2026-06-01 14:44:21 +0200

    SSR voor restaurant dynamic route via Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   site: 'https://www.tastory.nl',
+  output: 'hybrid',
+  adapter: vercel(),
   integrations: [sitemap()],
   build: {
     assets: 'assets',
