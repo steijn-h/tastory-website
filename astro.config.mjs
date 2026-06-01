f438b039680b526b11dd5590b7a6abf0d4e51e73
Steijn Holtkamp <steijn@mac.lan>
2026-06-01 14:47:00 +0200

    Fix: Astro 5 compatibele SSR config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   site: 'https://www.tastory.nl',
-  output: 'hybrid',
+  output: 'server',
   adapter: vercel(),
   integrations: [sitemap()],
   build: {
